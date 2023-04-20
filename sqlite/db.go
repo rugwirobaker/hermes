0752061324deaddf5c740f7d8310a5bf2efaf854
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2023-04-20 17:45:05 +0200

    Only run the clean up routine on our primary

--- sqlite/db.go
+++ sqlite/db.go
@@ -3,7 +3,10 @@ package sqlite
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	// _ "github.com/lib/pq"
 
@@ -16,7 +19,8 @@ import (
 var driver = "sqlite3"
 
 type DB struct {
-	db *sql.DB
+	db  *sql.DB
+	dsn string
 }
 
 func NewDB(dsn, server string, provider trace.TracerProvider) (*DB, error) {
@@ -34,7 +38,7 @@ func NewDB(dsn, server string, provider trace.TracerProvider) (*DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &DB{db}, nil
+	return &DB{db, dsn}, nil
 }
 
 // BeginTx starts a transaction and returns a wrapper Tx type. This type
@@ -63,6 +67,22 @@ func (db *DB) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
 		db:   db,
 		span: span,
 	}, nil
+}
+
+// IsPrimary checks if we're on the primary instance by reading the .primary file
+// that contains the primary instance's IP address. If the file doesn't exist we're either
+// on the primary instance or litefs is not running on the instance.
+func (db *DB) IsPrimary() (string, error) {
+	primaryFilename := filepath.Join(filepath.Dir(db.dsn), ".primary")
+
+	primary, err := os.ReadFile(primaryFilename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return "", nil
+		}
+		return "", err
+	}
+	return string(primary), nil
 }
 
 // Close closes the database connection.
